186b5ba248615cb4ea96eba49b84ba2ede21b6fe
nandinisharma1784-afk <nandinisharma1784@gmail.com>
2026-06-18 01:02:38 +0530

    Create day1for_loop(/n,end=(),***).py

---
+++ day1_(/n,end=(),***).py
@@ -0,0 +1,15 @@
+q1.
+for row in range(1, 4):         #outer loop (rows)
+    for col in range(1, 4):      # Inner loop (Columns)
+        
+        # 1. 'f' injects the row and col numbers into the text
+        # 2. end="\t" puts a tab space after it so columns line up cleanly
+        print(f"({row},{col})",end=("\t"))
+        
+    # This empty print() runs ONLY after the inner loop finishes a row.
+    # It has no custom 'end', so it uses the default end="\n" to press Enter.
+    print()
+
+
+
+q2
