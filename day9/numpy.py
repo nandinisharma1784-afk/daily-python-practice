104daaec0d87f4fa382fe8caa456aef5b9cef189
nandinisharma1784-afk <nandinisharma1784@gmail.com>
2026-06-29 23:19:37 +0530

    Create numpy.py with array examples
    Added various numpy array examples including 1D, 2D, 3D, zeros, arange, and linspace.

---
+++ day9/numpy.py
@@ -0,0 +1,17 @@
+!pip install numpy
+import numpy as np
+arr_1d=np.array([1,2,3,4])
+print(arr_1d)
+arr_2d=np.array([[3,4],[7,8]])
+print(arr_2d)
+arr_3d=np.array([[[1,2],[33,66]],[[55,6],[1,9]]])
+print(arr_3d)
+
+arr_0=np.zeros((100,100))
+print(arr_0)
+
+arr_arr=np.arange(0,6,1)
+print(arr_arr)
+
+arr_lin=np.linspace(0,10,5)
+print(arr_lin)
